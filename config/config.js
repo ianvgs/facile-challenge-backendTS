4f2f2887a523cf5e2e5dee67cbf64ce800f2b449
Ian Guedes <82589606+ianvgs@users.noreply.github.com>
2021-07-30 13:31:17 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -7,20 +7,29 @@
     "database": process.env.DATABASE,
     "port":process.env.DB_PORT,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions":{
+      "ssl":{"require":true,"rejectUnauthorized": false,}
+    }
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions":{
+      "ssl":{"require":true,"rejectUnauthorized": false,}
+    }
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions":{
+      "ssl":{"require":true,"rejectUnauthorized": false,}
+    }
   }
 }
