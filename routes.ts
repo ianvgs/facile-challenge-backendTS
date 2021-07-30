d6d1f32ee51ee9790eedbaed1bc7eeb2671dbd66
Ian Guedes <82589606+ianvgs@users.noreply.github.com>
2021-07-30 13:35:05 -0300

    Inclusão Get rota '/'.

--- routes.ts
+++ routes.ts
@@ -4,6 +4,9 @@ import UserController from './controllers/UserController'
 const routes = Router()
 
 routes.get('/registro/:id', UserController.index)
+routes.get('/', (req,res)=>{
+    res.send("Você acessou o projeto facile-backend, consulte o manual no README para verificar os métodos disponíveis.").status(200)
+})
 routes.post('/registro/', UserController.store)
 
 export default routes
