12b867595244ad59ce302719b7c7c57e95b05a5c
Ian Guedes <82589606+ianvgs@users.noreply.github.com>
2021-07-30 15:15:05 -0300

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -20,15 +20,15 @@ fs
   .filter((file: string) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach((file: any) => {
+  /*.forEach((file: any) => {
     const modelFile = path.join(__dirname, file);
     const model = sequelize['import'](modelFile)
     db[model.name] = model;
-  })/*
+  })*/
   .forEach((file: any) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
-  });*/
+  });
 
 
 
